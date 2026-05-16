cd3acdaf32bd97733fc20bcceecdcc3433747c70
Jhanh <quockhanh.nguyen2106@gmail.com>
2026-05-16 14:23:50 +0700

    Phân tích thừa số nguyên tố

---
+++ Basic/primeFactor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+void primeFactorization (long long n){
+    for (long long i = 2; i*i <= n; i++){
+        if (n%i == 0){
+            while (n%i == 0){
+                cout << i << ' ';
+                n /= i;
+            }
+        }
+    }
+    if (n > 1)  cout << n;
+}
+
+int main(){
+    long long n;
+    cin >> n;
+    primeFactorization(n);
+
+    return 0;
+}
