c389d01f82c357ac998bb58082cba6dda7ac9c8f
Jhanh <quockhanh.nguyen2106@gmail.com>
2026-05-18 16:38:30 +0700

    Rename folder Basic to Common

---
+++ Common/mergeSortArr.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using std::cin;
+using std::cout;
+using std::vector;
+
+// Time complexity: O(N + M)
+// Space complexity: O(N + M)
+
+int main(){
+    int n, m; cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) cin >> b[i];
+
+    int i = 0, j = 0; // i trỏ vào mảng a còn j trỏ vào mảng b
+    vector<int> v;
+    while (i < n && j < m){
+        if (a[i] <= b[j]){
+            v.push_back(a[i]);
+            ++i;
+        }
+        else{
+            v.push_back(b[j]);
+            ++j;
+        }
+    }
+    while (i < n){
+        v.push_back(a[i]);
+        ++i;
+    }
+    while (j < m){
+        v.push_back(b[j]);
+        ++j;
+    }
+
+    for (int x : v)  cout << x << ' ';
+
+    return 0;
+}
